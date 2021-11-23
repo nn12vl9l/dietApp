b4ac942675816194c34b982b84a8e55845c20d1d
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-11-23 10:10:07 +0900

    registerへのボタン作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -439,10 +439,10 @@
                                 class="w-full flex justify-center bg-gradient-to-r from-pink-500 to-purple-600 hover:bg-gradient-to-l hover:from-purple-500 hover:to-pink-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign
                                 in</a>
                         </div>
-                        <p class="flex flex-col items-center justify-center mt-10 text-center text-md">
+                        <p class="flex flex-col items-center justify-center mt-10 text-center text-md ">
                             <span>Don't have an account?</span>
                             <a href=/register
-                                class="hover:text-blue-500 no-underline hover:underline cursor-pointer transition ease-in duration-300">Sign
+                                class="mt-6 w-full flex justify-center bg-gradient-to-r from-green-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-green-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign
                                 up</a>
                         </p>
                     </div>
